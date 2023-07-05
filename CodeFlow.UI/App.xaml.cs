20d7dc9e518162317e5f45a59de872c55690ca0c
zhangzhonghua <956159241@qq.com>
2023-07-05 17:11:55 +0800

    初始化项目

---
+++ CodeFlow.UI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeFlow.UI
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+
+			Application.Current.UserAppTheme = AppTheme.Light;
+		}
+	}
+}
---
+++ CodeFlow.UI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CodeFlow.UI
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
