89b6f0739c1a6ce840087720a1fb46435c1bbb18
zhangzhonghua <956159241@qq.com>
2023-08-01 17:02:44 +0800

    名字改为前缀

--- electron/main/message/index.ts
+++ electron/main/message/index.ts
@@ -9,8 +9,8 @@ export const addMessageListener = async () => {
             const keys = Object.keys(module);
             keys.forEach((x: any) => {
                 try {
-                    if (module.default?.name && typeof module[x] === 'function') {
-                        ipcMain.on(`${module.default.name + ':' + x}`, module[x])
+                    if (module.default?.prefix && typeof module[x] === 'function') {
+                        ipcMain.on(`${module.default.prefix + ':' + x}`, module[x])
                     } else {
                         ipcMain.on(x, module[x]);
                     }
@@ -22,20 +22,3 @@ export const addMessageListener = async () => {
         }
     }
 };
-
-// for (const path in modules) {
-//     if (Object.prototype.hasOwnProperty.call(modules, path)) {
-//         const module:any = await modules[path]();
-//         const subApi = {};
-//         module.default.api.forEach(element => {
-//             if (element.name in api || element.name in subApi) console.error(`api 定义异常：${element.name}重复定义，请换个名字`);
-
-//             const prefix = getPrefix(module.default, element);
-//             subApi[element.name] = '/' + prefix + element.url;
-//         });
-
-//         api = { ...api, ...subApi };
-//     }
-// }
-
-// export default mess;
--- electron/main/message/window.ts
+++ electron/main/message/window.ts
@@ -1,4 +1,4 @@
-export default { name: 'window' };
+export default { prefix: 'window' };
 export const min  = (event, id) => {
     console.log(id);
 };
